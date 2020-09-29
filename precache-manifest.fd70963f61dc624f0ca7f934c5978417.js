a63d9cdcd47f5618c3a077f0a5bbd4d6edd199d9
subhampanja <subhampanja28@gmail.com>
2020-09-29 19:49:32 +0530

    Updates

---
+++ precache-manifest.fd70963f61dc624f0ca7f934c5978417.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b06cd6b9c9d4d0745150b3f3321e66f",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "613446ca1fa5a0f3de21",
+    "url": "/react-recipe-app/static/css/main.2c330a9f.chunk.css"
+  },
+  {
+    "revision": "a9e2a425753620e07a11",
+    "url": "/react-recipe-app/static/js/2.cae8a780.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-recipe-app/static/js/2.cae8a780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "613446ca1fa5a0f3de21",
+    "url": "/react-recipe-app/static/js/main.f9d53382.chunk.js"
+  },
+  {
+    "revision": "0353c6039341ff6b060f",
+    "url": "/react-recipe-app/static/js/runtime-main.36954690.js"
+  }
+]);
